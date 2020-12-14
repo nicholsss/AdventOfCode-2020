cc83879e4ae6b476e3eca46b269db06c9498d907
nicholsss <mickyaw1995@gmail.com>
2020-12-15 01:38:09 +0200

    p1 almost done

--- day03/one.js
+++ day03/one.js
@@ -1,16 +1,58 @@
 const fs = require("fs");
 function readInput() {
     const input = fs
-       // .readFileSync("input.txt").toString().split(/\r?\n/);
-       .readFileSync("input.txt").toString()
+        // .readFileSync("input.txt").toString().split(/\r?\n/);
+        .readFileSync("input.txt").toString()
     return input;
 }
+let row = 0;
+let tree = 0;
+function makeGrid(value) {
+    console.log(value)
+    let grid = value.split(/\r?\n/s).map(line => line.split(''))
+    console.log(grid.length)
+    for (let column = 0; column < grid[0].length; column++) {
 
-function makeGrid(value){
-console.log(value)
+        if (row == grid.length - 1) {
 
-let grid = value.split(/\r?\n/s).map(line => line.split(''))
-console.log(grid[1][3])
+            break;
+        }
+
+        let mod = column % 3;
+
+        if (mod == 3) {
+            row++;
+            if (grid[row][column] == "#") {
+                grid[row][column] = "X"
+                tree++;
+            } else {
+                grid[row][column] = "O"
+
+            }
+        } else {
+
+            row++;
+
+
+            let remain = mod;
+            console.log(remain)
+            if (grid[row][remain] == "#") {
+                grid[row][remain] = "X"
+                tree++;
+            } else {
+                grid[row][remain] = "O"
+            }
+
+        }
+
+
+    }
+    console.log(tree)
+
+
 }
 
 makeGrid(readInput());
+
+
+
