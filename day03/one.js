b7cf2964538b467a572eb1ea6f9bcf544d81fabd
nicholsss <mickyaw1995@gmail.com>
2020-12-15 11:54:31 +0200

    day3 done

--- day03/one.js
+++ day03/one.js
@@ -1,58 +1,34 @@
 const fs = require("fs");
 function readInput() {
-    const input = fs
-        // .readFileSync("input.txt").toString().split(/\r?\n/);
-        .readFileSync("input.txt").toString()
-    return input;
+  const input = fs.readFileSync("input.txt").toString();
+  return input;
 }
-let row = 0;
-let tree = 0;
 function makeGrid(value) {
-    console.log(value)
-    let grid = value.split(/\r?\n/s).map(line => line.split(''))
-    console.log(grid.length)
-    for (let column = 0; column < grid[0].length; column++) {
-
-        if (row == grid.length - 1) {
-
-            break;
-        }
-
-        let mod = column % 3;
-
-        if (mod == 3) {
-            row++;
-            if (grid[row][column] == "#") {
-                grid[row][column] = "X"
-                tree++;
-            } else {
-                grid[row][column] = "O"
-
-            }
-        } else {
-
-            row++;
-
-
-            let remain = mod;
-            console.log(remain)
-            if (grid[row][remain] == "#") {
-                grid[row][remain] = "X"
-                tree++;
-            } else {
-                grid[row][remain] = "O"
-            }
-
-        }
-
-
-    }
-    console.log(tree)
-
-
+  console.log(value);
+  let grid = value.split(/\r?\n/s).map((line) => line.split(""));
+  return grid;
 }
 
-makeGrid(readInput());
+function checkTrees(right, down, grid) {
+  let row = 0;
+  let tree = 0;
+  let col = 0;
+  console.log(right);
+  while (row < 323) {
+    if (grid[row][col] == "#") {
+      tree++;
+    }
 
-
-
+    col = (col + right) % 31;
+    row += down;
+  }
+  console.log("Trees found", tree);
+  return tree;
+}
+let grid = makeGrid(readInput());
+let trees = checkTrees(1, 1, grid);
+trees *= checkTrees(3, 1, grid);
+trees *= checkTrees(5, 1, grid);
+trees *= checkTrees(7, 1, grid);
+trees *= checkTrees(1, 2, grid);
+console.log("trees", trees);
